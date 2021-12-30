19e44ef6304c270cd29c27517ac17bbe53b3d1d6
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2021-12-30 12:10:06 +0700

    updated review

--- src/components/review-card/ReviewCard.jsx
+++ src/components/review-card/ReviewCard.jsx
@@ -2,12 +2,19 @@ import "./ReviewCard.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function ReviewCard(props){
+
+    let pfp = [
+        "https://static.zerochan.net/Texas.%28Arknights%29.full.2824483.jpg",
+        "https://preview.redd.it/1fd85j0at0f41.jpg?width=640&crop=smart&auto=webp&s=68c17507d15479937b184c4f9f6c5e86aea79f7f",
+        "https://cdn.donmai.us/original/ff/21/ff213428946865341e1e25c9852c0487.jpg"
+    ]
+
     return(
        <div className="d-flex flex-column review-wrapper">
            <div className="d-flex flex-row review">
-               <img src={props.review.profile} alt="" className="review__profile"/>
+               <img src={pfp[Math.floor(Math.random() * 3)]} alt="" className="review__profile"/>
                <div className="d-flex flex-column review__data">
-                   <h4>{props.review.name}</h4>
+                   <h4>{"Hans"}</h4>
                     <p>{props.review.description}</p>
                </div>
            </div>
--- src/pages/detail-page/DetailPage.jsx
+++ src/pages/detail-page/DetailPage.jsx
@@ -14,31 +14,43 @@ import WatchlistButton from "../../components/watchlist-button/WatchlistButton";
 
 function DetailPage() {
   let { id } = useParams();
-  const [animeId, setAnimeId] = useState(()=> "");
+  const [animeId, setAnimeId] = useState(() => "");
   let query = "";
+  let reviewScore = -1;
+  let description = "";
 
-  
   const [anime, setAnime] = useState(() => []);
+  const [review, setReview] = useState(() => []);
   const [fetchStatus, setFetchStatus] = useState(() => true);
   const [characterCount, setCharacterCount] = useState(() => 6);
-  const [fetchProgress, setFetchProgress] = useState(()=> 0);
-  
-  if(id !== animeId){
+  const [fetchProgress, setFetchProgress] = useState(() => 0);
+
+  if (id !== animeId) {
     setFetchStatus(true);
     setAnime({});
     setAnimeId(id);
   }
 
+  let ratingScore = ["-", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   useEffect(() => {
     const getAnime = async () => {
+      let URL = "http://localhost:8080/rating/view";
+      try {
+        const reviews = await axios.post(URL, {
+          animeID: animeId,
+        });
+        setReview(reviews.data);
+      } catch (error) {}
+
       let API_URL = "http://localhost:8080/anime/" + animeId;
-      console.log(API_URL);
       try {
         const anime = await axios.get(API_URL, {
-          onDownloadProgress: progressEvent => {
-              setFetchProgress(Math.floor((progressEvent.loaded / progressEvent.total) * 100));
-          }
-      });
+          onDownloadProgress: (progressEvent) => {
+            setFetchProgress(
+              Math.floor((progressEvent.loaded / progressEvent.total) * 100)
+            );
+          },
+        });
         setAnime(anime.data);
         setFetchStatus(false);
       } catch (error) {
@@ -48,32 +60,28 @@ function DetailPage() {
     getAnime();
   }, [query, animeId]);
 
-  let reviews = [
-    {
-      profile:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQGIbxivyhW18Q/profile-displayphoto-shrink_800_800/0/1632250683020?e=1645056000&v=beta&t=juY6_qRxSSAmWTMjWCrzKYGXqtkCNtyVQ3zZKdGOLnE",
-      name: "Hans",
-      description: "This is very good",
-      rating: "10",
-    },
-    {
-      profile:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQGIbxivyhW18Q/profile-displayphoto-shrink_800_800/0/1632250683020?e=1645056000&v=beta&t=juY6_qRxSSAmWTMjWCrzKYGXqtkCNtyVQ3zZKdGOLnE",
-      name: "Hans",
-      description: "This is amazing",
-      rating: "10",
-    },
-    {
-      profile:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQGIbxivyhW18Q/profile-displayphoto-shrink_800_800/0/1632250683020?e=1645056000&v=beta&t=juY6_qRxSSAmWTMjWCrzKYGXqtkCNtyVQ3zZKdGOLnE",
-      name: "Hans",
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusamus tempora ipsum distinctio, voluptas suscipit dignissimos placeat repellendus rerum tenetur nostrum nihil, unde molestiae corporis amet itaque explicabo dolore cum a aperiam quas magni laborum voluptatibus commodi! Nam, facilis adipisci.",
-      rating: "9",
-    },
-  ];
+  const addReview = async () => {
+    if(reviewScore === -1 || description.trim().length === 0){
+      return;
+    }
+    
+    document.getElementById("reviewModal").classList.remove("show", "d-block");
+    document.querySelectorAll(".modal-backdrop").forEach(bd => bd.classList.remove("modal-backdrop"));
 
-  console.log(anime)
+    let URL = "http://localhost:8080/rating/add";
+    try {
+      const review = await axios.post(URL, {
+        userID: 1,
+        animeID: animeId,
+        description: description,
+        rating: reviewScore
+      });
+      console.log(review);
+      setReview(prevReview => {
+        return [...prevReview, review.data]
+      });
+    } catch (error) {}
+  };
 
   const responsive = {
     desktop: {
@@ -94,29 +102,29 @@ function DetailPage() {
   };
 
   const loadCharacter = () => {
-        setCharacterCount((count) => count + 6);
-    };
+    setCharacterCount((count) => count + 6);
+  };
 
-    const hideCharacter = () => {
-        setCharacterCount(6);
-    };
+  const hideCharacter = () => {
+    setCharacterCount(6);
+  };
 
-    return (
-        <div key={id} className="detail-page">
-          <LoadingBar
-                color='#44B9DE'
-                progress={fetchProgress}
-                height={3}
-                transitionTime={100}
-                loaderSpeed={400}
-                waitingTime={500}
-                onLoaderFinished={() => setFetchProgress(0)}
-            />
-        {!fetchStatus ? (
-            <div className="detail-content">
-            <div className="d-flex flex-row align-items-center information-section custom-container">
-                <img src={anime.image_url} alt="" className="anime-poster" />
-                <div className="d-flex flex-column anime-detail">
+  return (
+    <div key={id} className="detail-page">
+      <LoadingBar
+        color="#44B9DE"
+        progress={fetchProgress}
+        height={3}
+        transitionTime={100}
+        loaderSpeed={400}
+        waitingTime={500}
+        onLoaderFinished={() => setFetchProgress(0)}
+      />
+      {!fetchStatus ? (
+        <div className="detail-content">
+          <div className="d-flex flex-row align-items-center information-section custom-container">
+            <img src={anime.image_url} alt="" className="anime-poster" />
+            <div className="d-flex flex-column anime-detail">
               <h1>{anime.title}</h1>
               <div className="information-section__second align-items-center d-inline-flex">
                 <span className="d-flex align-items-center genre-container">
@@ -143,7 +151,7 @@ function DetailPage() {
                   <p>{anime.score ? anime.score : "N/A"}</p>
                 </span>
               </div>
-              <WatchlistButton source={"detail"} id={id}/>
+              <WatchlistButton source={"detail"} id={id} />
               <div className="information-section__fourth">
                 <p className="description-section">
                   {anime.synopsis
@@ -199,19 +207,80 @@ function DetailPage() {
           <div className="d-flex flex-column review-section custom-container">
             <h1>Review</h1>
             <div className="review-section__review d-flex flex-column">
-              {reviews.map((review) => (
+              {review.map((review) => (
                 <ReviewCard review={review} />
               ))}
             </div>
-            <button className="d-flex flex-row align-items-center load-button">
-              <FontAwesomeIcon
-                icon={["fas", "chevron-down"]}
-                style={{ color: "#44C1E9" }}
-              />
-              <p>Load More</p>
-            </button>
+            <div className="review-buttons d-flex flex-row align-items-center justify-content-center">
+              <button className="d-flex flex-row align-items-center load-button">
+                <FontAwesomeIcon
+                  icon={["fas", "chevron-down"]}
+                  style={{ color: "#44C1E9" }}
+                />
+                <p>Load More</p>
+              </button>
+              <button
+                type="button"
+                className="d-flex flex-row align-items-center load-button"
+                data-bs-toggle="modal"
+                data-bs-target="#reviewModal"
+              >
+                <FontAwesomeIcon
+                  icon={["fas", "plus"]}
+                  style={{ color: "#44C1E9" }}
+                />
+                <p>Add Review</p>
+              </button>
+            </div>
           </div>
-
+          <div
+            class="modal fade"
+            id="reviewModal"
+            tabindex="-1"
+            aria-labelledby="reviewModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="custom-modal modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text--blue" id="reviewModalLabel">
+                    Review
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close btn-close-white"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="custom-body modal-body d-flex flex-column">
+                  <div className="score-section d-flex flex-row align-items-center">
+                    <h7 className="text--white">Rating: </h7>
+                      <select name="score-select" class="score-cbo" onClick={(e)=>{reviewScore = e.target.value !== "-" ? e.target.value : -1}}>
+                        {
+                          ratingScore.map(score => 
+                            <option value={score}>{score}</option>)
+                        }
+                      </select>
+                  </div>
+                  <h7 className="text--white">Description:</h7>
+                  <textarea name="" id="" rows="10" onChange={(e)=>{description = e.target.value}}></textarea>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Cancel
+                  </button>
+                  <button type="button" class="btn btn-primary" onClick={addReview}>
+                    Add Review
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="recommendation-section custom-container d-flex flex-column">
             <div className="d-flex flex-row">
               <h1>More&nbsp;</h1>
@@ -229,7 +298,11 @@ function DetailPage() {
                 itemClass="carousel-item-padding-40-px"
               >
                 {anime.recommendations.map((recommendation) => (
-                  <AnimeCard anime={recommendation} type={"recommendation"} source={"detail"} />
+                  <AnimeCard
+                    anime={recommendation}
+                    type={"recommendation"}
+                    source={"detail"}
+                  />
                 ))}
               </Carousel>
             </div>
